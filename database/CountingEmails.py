c4657e000fd8f28aeb7ed88165224c44355fbea7
r-sreesaran <r.sreesaran@gmail.com>
2018-04-25 13:04:47 +0530

    added email reader

--- database/CountingEmails.py
+++ database/CountingEmails.py
@@ -1,17 +1,29 @@
 import sqlite3
+import urllib.request
 
 con = sqlite3.connect('test.sqlite')
 cur = con.cursor()
 
 cur.execute('''DROP TABLE IF EXISTS Org''')
-cur.execute(''' CREATE TABLE  Org (email TEXT, count INTEGER)''')
+cur.execute(''' CREATE TABLE  Org (person  int, count varchar(255))''')
+fname = 'mbox.txt'
+fh = open(fname)
 
-file = open('mbox.txt')
-
-for line in file:
+for line in fh:
     if not line.startswith('From'): continue
     words = line.split()
-    email = words[1]
-    row = cur.fetchone()
+    email = words[1].split('@')[1]
     
-    
+    if  cur.fetchone is None:
+        cur.execute('''INSERT INTO Org(email,count) VALUES(?,1) ''',(email,))
+    else:
+        cur.execute('''UPDATE Org SET count = count + 1 WHERE email=? ''',(email,))
+
+con.commit()
+   
+sqlstr = 'SELECT * FROM Org '
+
+for row in con.execute(sqlstr):
+    print(row[0],row[1])  
+
+  
--- database/DataBaseReader.py
+++ database/DataBaseReader.py
@@ -1,37 +1,35 @@
 import sqlite3
+import time
 
-conn = sqlite3.connect('emaildb.sqllite')
+starttime = time.time()
+conn = sqlite3.connect('orgdb.sqlite')
 cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
 cur.execute('''
-DROP TABLE IF EXISTS Counts
-''')
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
-cur.execute(''' 
-CREATE TABLE Counts (email TEXT, count INTEGER)
-''')
-
-#fname = input('Enter file name:')
-#if(len(fname)< 1 ): 
-fname = 'mbox-short.txt'
+fname = 'mbox.txt'
 fh = open(fname)
-
 for line in fh:
-    if not line.startswith('From '): continue
+    if not line.startswith('From: '): continue
     pieces = line.split()
-    email = pieces[1]
+    org = pieces[1].split('@')[1]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
     row = cur.fetchone()
-
     if row is None:
-        cur.execute('''INSERT INTO Counts(email, count) VALUES (?,1)''',(email,))
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
     else:
-        cur.execute('''UPDATE Counts SET count = count + 1  WHERE email = ?  ''',(email,))
-
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
     conn.commit()
 
-sqlstr = 'SELECT * FROM Counts '
+print('The end time is',time.time() - starttime,'seconds')    
+sqlstr = 'SELECT org, count FROM Counts'
 
-#print(cur.execute(sqlstr))
-for row in cur.execute(sqlstr):
-   print(row[0],row[1])
+#for row in cur.execute(sqlstr):
+#    print(str(row[0]), row[1])
 
-cur.close
+cur.close()
